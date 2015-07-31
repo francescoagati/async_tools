4c45cd65c55cad17cb6dc70fd0d3affa643c6b9f
Francesco Agati <fagati@veespo.com>
2015-07-31 16:53:04 +0200

    add wait for stream

--- src/async_tools/Stream.hx
+++ src/async_tools/Stream.hx
@@ -4,10 +4,10 @@ using promhx_tools.StreamTools;
 
 class Stream {
 
-  inline static function wait_for_stream<T>(stream:promhx.Stream<T>,cb:T->Void) {
+  public inline static function wait_for_stream<T>(stream:promhx.Stream<T>,cb:T->Void) {
     stream.take(1).then(function(value) {
       cb(value);
     });
   }
-  
+
 }
