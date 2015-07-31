8c874341535d71cf6b6e7da664d810eebf19cc2c
Francesco Agati <fagati@veespo.com>
2015-07-31 16:30:17 +0200

    add wait for stream

--- bin/async.js
+++ bin/async.js
@@ -12,5 +12,11 @@ var async_$tools_Main = function() { };
 async_$tools_Main.__interfaces__ = [async_$tools_Async];
 async_$tools_Main.main = function() {
 };
+function $iterator(o) { if( o instanceof Array ) return function() { return HxOverrides.iter(o); }; return typeof(o.iterator) == 'function' ? $bind(o,o.iterator) : o.iterator; }
+var $_, $fid = 0;
+function $bind(o,m) { if( m == null ) return null; if( m.__id__ == null ) m.__id__ = $fid++; var f; if( o.hx__closures__ == null ) o.hx__closures__ = {}; else f = o.hx__closures__[m.__id__]; if( f == null ) { f = function(){ return f.method.apply(f.scope, arguments); }; f.scope = o; f.method = m; o.hx__closures__[m.__id__] = f; } return f; }
+var q = window.jQuery;
+var js = js || {}
+js.JQuery = q;
 async_$tools_Main.main();
 })(typeof console != "undefined" ? console : {log:function(){}});
--- src/async_tools/Cps.hx
+++ src/async_tools/Cps.hx
@@ -1,4 +1,6 @@
 package async_tools;
+using promhx_tools.StreamTools;
+
 
 #if (macro || neko)
   import haxe.macro.Expr;
@@ -32,5 +34,14 @@ class Cps {
   public macro static function cont_exec(body:Expr) {
     return macro async_tools.Cps.cont($e{body})(function() {});
   }
+
+
+  inline static function wait_for_stream<T>(stream:promhx.Stream<T>,cb:T->Void) {
+    stream.take(1).then(function(value) {
+      cb(value);
+    });
+  }
+
+
 
 }
